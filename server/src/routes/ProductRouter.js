af322b693ab6729a7380f4e3ce13a480d9e3a2ac
thanhtai22 <tai127589@gmail.com>
2023-10-13 00:08:06 +0700

    Product-getall,details,delete,update

--- server/src/routes/ProductRouter.js
+++ server/src/routes/ProductRouter.js
@@ -1,16 +1,11 @@
 const express = require("express");
 const router = express.Router()
-const userController = require('../controllers/UserController');
-const { authMiddleWare, authUserMiddleWare } = require("../middleware/authmiddleware");
+const ProductController = require("../controllers/ProductController");
+const { authMiddleWare } = require("../middleware/authmiddleware");
 
-router.post('/reg', userController.createUser)
-router.post('/login', userController.loginUser)
-router.post('/logout', userController.logoutUser)
-router.put('/update-user/:id', userController.updateUser)
-router.delete('/delete-user/:id',authMiddleWare, userController.deleteUser)
-router.get('/getAll',authMiddleWare, userController.getAllUser)
-router.get('/get-details/:id', authUserMiddleWare, userController.getDetailsUser)
-router.post('/refresh-token', userController.refreshToken)
-router.post('/delete-many', authMiddleWare, userController.deleteMany)
-
+router.post('/create', ProductController.createProduct)
+router.put('/update/:id',authMiddleWare, ProductController.updateProduct)
+router.get('/getdetails/:id', ProductController.getDetailsProduct)
+router.delete('/delete/:id', ProductController.deleteProduct)
+router.delete('/get-all',authMiddleWare, ProductController.getAllProduct)
 module.exports = router
